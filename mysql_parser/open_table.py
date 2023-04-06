33b3219f088c9fb9b7f803fb20ddd6c448389267
Christopher Morse <chris.morse.98@gmail.com>
2023-04-05 21:15:47 -0400

    Parser update

--- mysql_parser/open_table.py
+++ mysql_parser/open_table.py
@@ -1,16 +1,15 @@
 import mysql.connector
 import pyshark
 
-# Connect to MySQL server
+# Connection to mysql server (enter your mysql info in these fields)
 cnx = mysql.connector.connect(host='localhost', user='root', password='password')
 cursor = cnx.cursor()
 
-if cnx.is_connected():
-    print('Connected to MySQL database')
+# fetch and print rows from the table
+
 cursor.execute(""" USE pcap_data """)
 cursor.execute(""" SELECT * FROM packets """)
 
-# fetch and print rows from the table
-data = cursor.fetchall ()
+data = cursor.fetchall()
 for row in data:
     print(row)
--- mysql_parser/parse_pcap.py
+++ mysql_parser/parse_pcap.py
@@ -1,24 +1,23 @@
 import mysql.connector
 import pyshark
 
-# Connect to MySQL server
+# Connection to mysql server (enter your mysql info in these fields)
 cnx = mysql.connector.connect(host='localhost', user='root', password='password')
 cursor = cnx.cursor()
 
-# Create new database if it doesn't already exist
+# Create new database if it doesn't already exist, named 'pcap_data'
 cursor.execute("CREATE DATABASE IF NOT EXISTS pcap_data")
 cnx.database = 'pcap_data'
 
-# Create packets table if it doesn't already exist
+# Create a table in the database named 'packets' (overwrites!)
 cursor.execute("DROP TABLE IF EXISTS packets")
 cursor.execute("CREATE TABLE packets (id INT AUTO_INCREMENT PRIMARY KEY, src_ip VARCHAR(15), dst_ip VARCHAR(15), src_port INT, dst_port INT, protocol VARCHAR(10), length INT, timestamp DOUBLE, syn_flag INT, ack_flag INT, fin_flag INT, handshake VARCHAR(200), record VARCHAR(200))")
 
-# Parse pcap file using PyShark
+# Iterate through pcap file and insert into table
 cap = pyshark.FileCapture('part1.pcap')
 
-# Iterate through packets and insert into database
+# For each packet...
 for pkt in cap:
-    # Extract relevant fields
     timestamp = float(pkt.sniff_time.timestamp())
     src_ip = str(pkt.ip.src)
     dst_ip = str(pkt.ip.dst)
@@ -27,16 +26,14 @@ for pkt in cap:
     length = int(pkt.length)
     protocol = str(pkt.transport_layer)
 
-    syn_flag = -1
-    ack_flag = -1
-    fin_flag = -1
-    handshake = "None"
-    record = "None"
+    syn_flag = ack_flag = fin_flag = -1
+    handshake = record = ""
 
     if "TCP" in str(pkt.layers):
         syn_flag = int(pkt.tcp.flags_syn)
         ack_flag = int(pkt.tcp.flags_ack)
         fin_flag = int(pkt.tcp.flags_fin)
+
     if "TLS" in str(pkt.layers):
         if "handshake" in pkt.tls.field_names:
             #print(pkt.tls.field_names)
@@ -45,7 +42,7 @@ for pkt in cap:
         if "record" in pkt.tls.field_names:
             record = str(pkt.tls.record)
 
-    # Insert packet into database
+    # Insert info into database
     query = "INSERT INTO packets (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     values = (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record)
     cursor.execute(query, values)
