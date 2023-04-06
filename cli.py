d604846cc0c40c0a964daeaee2586d580c60f1ce
Christopher Morse <chris.morse.98@gmail.com>
2023-04-05 21:46:25 -0400

    improved database connection flow

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from mysql_parser.parse_pcap import *
 
 parser = argparse.ArgumentParser(
                     prog='Pcap filter with GPT',
@@ -10,6 +11,7 @@ parser.add_argument(
     "--pcap",
     type=Path,
     required=True,
+    dest='pcap',
     help="Path to the pcap file."
 )
 parser.add_argument(
@@ -17,14 +19,30 @@ parser.add_argument(
     "--query",
     type=str,
     required=True,
+    dest='query',
     help="Query in natural language."
 )
 
 def main():
-    print("Should parse the pcap file")
-    print("Should pass text to GPT and get a mysql query")
-    print("Should infer the database with the mysql query")
-    print("Show results")
+    args = parser.parse_args()
+    # Connect and initialize MySQL database
+    cnx, cursor = init_database("localhost", "root", "password")
+
+    # Parse the pcap file
+    convert_pcap_to_table(args.pcap, cnx, cursor)
+
+    # TODO: Pass text to GPT and get a mysql query
+
+
+    # Send query to table and retrieve result
+    query = """ SELECT * FROM packets """
+    result = query_table(cursor, query)
+
+    print(result)
+
+    # Close connection
+    cursor.close()
+    cnx.close()
 
 if __name__ == "__main__":
     main()
--- mysql_parser/parse_pcap.py
+++ mysql_parser/parse_pcap.py
@@ -1,53 +1,63 @@
 import mysql.connector
 import pyshark
 
-# Connection to mysql server (enter your mysql info in these fields)
-cnx = mysql.connector.connect(host='localhost', user='root', password='password')
-cursor = cnx.cursor()
+def init_database(host_name, username, pw):
+    # Connection to mysql server (enter your mysql info in these fields)
+    cnx = mysql.connector.connect(host=host_name, user=username, password=pw)
+    cursor = cnx.cursor()
 
-# Create new database if it doesn't already exist, named 'pcap_data'
-cursor.execute("CREATE DATABASE IF NOT EXISTS pcap_data")
-cnx.database = 'pcap_data'
+    # Create new database if it doesn't already exist, named 'pcap_data'
+    cursor.execute("CREATE DATABASE IF NOT EXISTS pcap_data")
+    cnx.database = 'pcap_data'
 
-# Create a table in the database named 'packets' (overwrites!)
-cursor.execute("DROP TABLE IF EXISTS packets")
-cursor.execute("CREATE TABLE packets (id INT AUTO_INCREMENT PRIMARY KEY, src_ip VARCHAR(15), dst_ip VARCHAR(15), src_port INT, dst_port INT, protocol VARCHAR(10), length INT, timestamp DOUBLE, syn_flag INT, ack_flag INT, fin_flag INT, handshake VARCHAR(200), record VARCHAR(200))")
+    # Create a table in the database named 'packets' (overwrites!)
+    cursor.execute("DROP TABLE IF EXISTS packets")
+    cursor.execute("CREATE TABLE packets (id INT AUTO_INCREMENT PRIMARY KEY, src_ip VARCHAR(15), dst_ip VARCHAR(15), src_port INT, dst_port INT, protocol VARCHAR(10), length INT, timestamp DOUBLE, syn_flag INT, ack_flag INT, fin_flag INT, handshake VARCHAR(200), record VARCHAR(200))")
+    return cnx, cursor
 
-# Iterate through pcap file and insert into table
-cap = pyshark.FileCapture('part1.pcap')
 
-# For each packet...
-for pkt in cap:
-    timestamp = float(pkt.sniff_time.timestamp())
-    src_ip = str(pkt.ip.src)
-    dst_ip = str(pkt.ip.dst)
-    src_port = int(pkt[pkt.transport_layer].srcport)
-    dst_port = int(pkt[pkt.transport_layer].dstport)
-    length = int(pkt.length)
-    protocol = str(pkt.transport_layer)
+def convert_pcap_to_table(file, cnx, cursor):
+    # Iterate through pcap file and insert into table
+    cap = pyshark.FileCapture(file)
 
-    syn_flag = ack_flag = fin_flag = -1
-    handshake = record = ""
+    # For each packet...
+    for pkt in cap:
+        timestamp = float(pkt.sniff_time.timestamp())
+        src_ip = str(pkt.ip.src)
+        dst_ip = str(pkt.ip.dst)
+        src_port = int(pkt[pkt.transport_layer].srcport)
+        dst_port = int(pkt[pkt.transport_layer].dstport)
+        length = int(pkt.length)
+        protocol = str(pkt.transport_layer)
 
-    if "TCP" in str(pkt.layers):
-        syn_flag = int(pkt.tcp.flags_syn)
-        ack_flag = int(pkt.tcp.flags_ack)
-        fin_flag = int(pkt.tcp.flags_fin)
+        syn_flag = ack_flag = fin_flag = -1
+        handshake = record = ""
 
-    if "TLS" in str(pkt.layers):
-        if "handshake" in pkt.tls.field_names:
-            #print(pkt.tls.field_names)
-            handshake = str(pkt.tls.handshake)
+        if "TCP" in str(pkt.layers):
+            syn_flag = int(pkt.tcp.flags_syn)
+            ack_flag = int(pkt.tcp.flags_ack)
+            fin_flag = int(pkt.tcp.flags_fin)
 
-        if "record" in pkt.tls.field_names:
-            record = str(pkt.tls.record)
+        if "TLS" in str(pkt.layers):
+            if "handshake" in pkt.tls.field_names:
+                #print(pkt.tls.field_names)
+                handshake = str(pkt.tls.handshake)
 
-    # Insert info into database
-    query = "INSERT INTO packets (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record)
-    cursor.execute(query, values)
-    cnx.commit()
+            if "record" in pkt.tls.field_names:
+                record = str(pkt.tls.record)
 
-# Close database connection
-cursor.close()
-cnx.close()
+        # Insert info into database
+        query = "INSERT INTO packets (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (src_ip, dst_ip, src_port, dst_port, protocol, length, timestamp, syn_flag, ack_flag, fin_flag, handshake, record)
+        cursor.execute(query, values)
+        cnx.commit()
+
+
+def query_table(cursor, query):
+    # Execute the given query
+    cursor.execute(""" USE pcap_data """)
+    cursor.execute(query)
+
+    # Retrieve and return results
+    data = cursor.fetchall()
+    return data
